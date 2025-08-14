9512fca66dadbbca7b7f06368f459d6f4b844a8d
Manukula <112695700+Manukula@users.noreply.github.com>
2025-08-14 19:36:04 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+ {
+            question: "What does CPU stand for?",
+            answers: [
+                { text: "Central Processing Unit", correct: true },
+                { text: "Computer Personal Unit", correct: false },
+                { text: "Central Personal Unit", correct: false },
+                { text: "Computer Processing Unit", correct: false }
+            ]
+        }
+        {
+            question: "What is the main function of RAM?",
+            answers: [
+                { text: "Store data permanently", correct: false },
+                { text: "Provide temporary storage for data in use", correct: true },
+                { text: "Process graphics", correct: false },
+                { text: "Control input devices", correct: false }
+            ]
+        }
